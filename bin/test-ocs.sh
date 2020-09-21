155572ba3139c562cc609bde2ab74fd0f748d444
Mrugesh Karnik <mkarnik@redhat.com>
2020-09-21 23:23:07 +0530

    Fixes to kubeconfig and cluster directory setup

--- bin/test-ocs.sh
+++ bin/test-ocs.sh
@@ -5,9 +5,10 @@ set -e
 # Set default locations if not available in the environment.
 # These are configured in the Dockerfile by default.
 CLUSTER_DIR="${CLUSTER_DIR:=/opt/cluster}"
+CLUSTER_KUBECONFIG="${CLUSTER_DIR}/auth/kubeconfig"
 OCSCI_INSTALL_DIR="${OCSCI_INSTALL_DIR:=/opt/ocs-ci}"
 OUTPUT_DIR="${OUTPUT_DIR:=/test-run-results}"
-TOOLBOX_POD_YAML="${TOOLBOX_POD_YAML:=$OCSCI_INSTALL_DIR/ocs_ci/templates/ocs-deployment/toolbox_pod.yaml}"
+TOOLBOX_POD_YAML="${TOOLBOX_POD_YAML:=${OCSCI_INSTALL_DIR}/ocs_ci/templates/ocs-deployment/toolbox_pod.yaml}"
 
 # Location of the junit output file.
 JUNIT_XML="${OUTPUT_DIR}/junit.xml"
@@ -16,25 +17,30 @@ JUNIT_XML="${OUTPUT_DIR}/junit.xml"
 echo "### Setup script variables."
 echo "- TOOLBOX_POD_YAML: $TOOLBOX_POD_YAML"
 echo "- OCSCI_INSTALL_DIR: $OCSCI_INSTALL_DIR"
-echo "- KUBE_CLUSTER_NAME: $KUBE_CLUSTER_NAME"
 echo "- CLUSTER_DIR: $CLUSTER_DIR"
+echo "- CLUSTER_KUBECONFIG: $CLUSTER_KUBECONFIG"
 echo "- JUNIT_XML: $JUNIT_XML"
 echo
 
 # Create kubeconfig.
 echo "### Setting up kubeconfig."
 SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
-echo -n "- " && kubectl config set clusters.ocs.server https://kubernetes.default.svc
-echo -n "- " && kubectl config set clusters.ocs.certificate-authority-data $(base64 -i -w 0 <"$SERVICEACCOUNT/ca.crt")
-echo -n "- " && kubectl config set users.cluster-admin.client-key-data $(base64 -i -w 0 <"$SERVICEACCOUNT/token")
-echo -n "- " && kubectl config set contexts.ocs.cluster ocs
-echo -n "- " && kubectl config set contexts.ocs.user cluster-admin
-echo -n "- " && kubectl config set current-context ocs
-echo
+echo -n "- " && \
+  kubectl config set-cluster ocs-test \
+    --server=https://kubernetes.default.svc \
+    --certificate-authority="${SERVICEACCOUNT}/ca.crt" \
+    --embed-certs
+echo -n "- " && \
+  kubectl config set-credentials cluster-admin --token=$(<${SERVICEACCOUNT}/token)
+echo -n "- " && \
+  kubectl config set-context ocs-test \
+    --cluster ocs-test \
+    --user cluster-admin
+echo -n "- " && \
+  kubectl config use-context ocs-test
 
-kubectl config view --raw > /kubeconfig
-
-export KUBECONFIG=/kubeconfig
+export KUBECONFIG="${HOME}/.kube/config"
+KUBE_CLUSTER_NAME="ocs-test"
 
 # Debug output to stdout to be captured into artifacts by the harness.
 echo "### kubeconfig content in '$KUBECONFIG'."
@@ -42,9 +48,15 @@ echo
 cat "$KUBECONFIG"
 echo
 
+# Copy kubeconfig to cluster directory.
+echo "### Setting up the cluster directory."
+echo
+echo -n "- " && mkdir -pv "${CLUSTER_DIR}"/{auth,logs}
+echo -n "- " && cp -v "$KUBECONFIG" "$CLUSTER_KUBECONFIG"
+
 # Create the ceph tools pod.
 echo "### Creating the ceph tools pod."
-kubectl create -f "$TOOLBOX_POD_YAML"
+kubectl --kubeconfig="$CLUSTER_KUBECONFIG" create -f "$TOOLBOX_POD_YAML"
 echo
 sleep 5
 
