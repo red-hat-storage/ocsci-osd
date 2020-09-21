20212b1cc0719c2304c063c6eb5c39264292d0d4
Mrugesh Karnik <mkarnik@redhat.com>
2020-09-21 23:27:07 +0530

    Script to access k8s api using curl inside the pod

---
+++ bin/kube-api.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Setup k8s auth for API access
+# Copied over from https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/#accessing-the-api-from-within-a-pod
+
+# Point to the internal API server hostname
+APISERVER=https://kubernetes.default.svc
+
+# Path to ServiceAccount token
+SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+
+# Read this Pod's namespace
+NAMESPACE=$(cat ${SERVICEACCOUNT}/namespace)
+
+# Read the ServiceAccount bearer token
+TOKEN=$(cat ${SERVICEACCOUNT}/token)
+
+# Reference the internal certificate authority (CA)
+CACERT=${SERVICEACCOUNT}/ca.crt
+
+# Explore the API with TOKEN
+curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api
+
